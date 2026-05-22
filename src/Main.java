e2ec70ea982a059d554827e19fe386e21ba6e3b9
Carlos Reyes <reyescarlos691@gmail.com>
2026-05-22 11:54:46 +0200

    Reto Integrador: Sistema de recargas VIP

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,29 @@
-import java.util.Scanner; //Traer la herramienta de escucha
+import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args){
-        //RETO 1: EL CONTROL DE PUERTA (IF/ESLSE)
-
-        // Se crea el micrófono llamado teclado o leer
+    public static void main (String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        System.out.println("Por favor, ingrese la edad del cliente:");
+        System.out.println("Hola, Por favor ingresa tu nombre completo: ");
+        String nombre = teclado.nextLine();
 
-        //Atrapamos el dato ingresado por el usuario
-        int edad = teclado.nextInt();
+        System.out.println("Cuanto dinero quieres recargar en tu tarjeta de juego?: ");
+        double recarga = teclado.nextDouble();
 
-        if (edad < 18) {
-            System.out.println("Acceso Denegado!");
+        double saldo = 0;
+
+        if (recarga < 20) {
+            saldo = recarga;
+            System.out.println("Muchas gracias por tu recarga de " + recarga + " Mucha Suerte!");
+        } else if (recarga >= 20 && recarga <50) {
+            saldo = recarga + (recarga * 0.10);
+            System.out.println("Muchas gracias por tu recarga de " + recarga + " Ten un Regalo del 10% tu saldo ahora es de: " + saldo + " Mucha Suerte!");
         }
-        else if (edad == 18){
-            System.out.println("Acceso permitido. ¡Felicidades por tu mayoría de edad, tienes una consumición gratis!");
+        else if (recarga >= 50) {
+            saldo = recarga + (recarga * 0.20);
+            System.out.println("Muchas gracias por tu recarga de " + recarga +
+                    " Ten un Regalo del 20% tu saldo ahora es de: " + saldo + "¡ Tienes derecho a una bebida gratis en barra! Mucha Suerte!");
         }
-        else {
-            System.out.println("Acceso permitido. Bienvenido al salón!");
-        }
-        teclado.close(); // Recomendado profesionalmente para liberar memoria, ya que apaga el teclado
+        System.out.println("Muchas gracias por usar nuestras plataformas de recarga: " + nombre + " Tu saldo total es de: " + saldo);
     }
 }
