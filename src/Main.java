5a16040cd022d966e2b6d54f37e3529e2c8d7a3c
Carlos Reyes <reyescarlos691@gmail.com>
2026-05-30 17:51:59 +0200

    Reto ARBOLITO CON ASCII

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,36 @@
-import java.util.Scanner;
-
 public class Main {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
+        //RETO DE ARBOLITO CON ASCII
 
-        // RETO MARATÓN DEL INMIGRANTE
-        // pedir datos a las personas dni, nombre y edad
-        // determinar a que categoria se tiene que inscribir
-        //informarle a la persona
-        // no sabemos la cantidad de personas
+        // Declarar la altura del arbol
+        int altura = 20;
 
-        Scanner teclado = new Scanner(System.in);
+        //bucle para recorrer las filas (altura)
+        for ( int fila=0; fila<altura; fila++) {
 
-        System.out.println("Ingrese el DNI de la persona: ");
-        String dni = teclado.nextLine();
-
-        System.out.println("Ingrese el Nombre de la persona: ");
-        String nombre = teclado.nextLine();
-
-        System.out.println("Ingrese la Edad de la persona: ");
-        int edad = Integer.parseInt(teclado.nextLine());
-
-        while (!dni.equals("0") && !nombre.equalsIgnoreCase("fin")) {
-
-            if (edad >= 6 && edad <= 10) {
-                System.out.println("Pertenece a la categoria de Menores A");
-            } else if (edad >= 11 && edad <= 17) {
-                System.out.println("Pertenece a la categoria de  Menores B");
-            } else if (edad >= 18 && edad <= 30) {
-                System.out.println("Pertenece a la categoria de Juveniles");
-            } else if (edad >= 31 && edad <= 50) {
-                System.out.println("Pertenece a la categoria de Adultos");
-            } else if (edad >= 51) {
-                System.out.println("Pertenece a la categoria de Adultos Mayores");
-            } else {
-                System.out.println("Edad invalida, debes tener 6 años en adelante para poder inscribirte");
+            //bucle para los espacios
+            for (int espacio=0; espacio<(altura-fila-1); espacio++) {
+                System.out.print(" ");
             }
-
-            System.out.println("Ingrese el DNI de la persona: ");
-            dni = teclado.nextLine();
-
-            System.out.println("Ingrese el Nombre de la persona: ");
-            nombre = teclado.nextLine();
-
-            System.out.println("Ingrese la Edad de la persona: ");
-            edad = Integer.parseInt(teclado.nextLine());
+            //bucle para los asteriscos
+            for (int asterisco=0; asterisco<(fila*2)+1; asterisco++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
+        // Tronco
+        int largoTronco = 1;
+        for (int base=0; base<largoTronco; base++) {
+
+        }
+        // Espacios Blanco
+        for (int espacio=0; espacio<(altura-2); espacio++ ){
+            System.out.print(" ");
+        }
+        //Barritas tronco
+        for (int tronco=0; tronco<3; tronco++) {
+            System.out.print("|");
+        }
+        System.out.println("");
     }
 }
