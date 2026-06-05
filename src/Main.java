87e2b59b253797962185e5e952766994603db3bd
Carlos Reyes <reyescarlos691@gmail.com>
2026-06-05 18:40:08 +0200

    Matrices manuales y por asignación

--- src/Main.java
+++ src/Main.java
@@ -2,68 +2,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args) {
-    /*
-        // Vectores
-        // Declaración
-        int vector[] = new int[4];
+
+        //MATRICES
+        //Declaracion
+        int matriz [][] = new int [3] [3];
 
        /* //Asignación manual
-        vector[0] = 2;
-        vector[1] = 35;
-        vector[2] = 48;
-        vector[3] = 157;*/
+        matriz [0][0] = 5;
+        matriz [0][1] = 13;
+        matriz [0][2] = 96;
+        matriz [1][0] = 35;
+        matriz [1][1] = 33;
+        matriz [1][2] = 71;
+        matriz [2][0] = 446;
+        matriz [2][1] = 228;
+        matriz [2][2] = 24; */
 
-        //Asignacion por teclado
-       /* Scanner teclado = new Scanner(System.in);
-        for (int i=0; i<vector.length; i++) {
-            System.out.println("Ingrese el valor para el indice " + i);
-            vector[i] = Integer.parseInt(teclado.nextLine());
-        }
-
-        // Recorrido
-        for (int i=0; i<vector.length; i++) {
-            System.out.println("Estoy en el índice: " + i);
-            System.out.println("Tengo guardado un " + vector[i]);
-            System.out.println("________________________");
-        }
-        teclado.close; */
-
-        // 2. EJERCICIO INTEGRADOR REGISTRO DE TEMPERATURAS
-        // El programa debe pedirle al usuario las 5 temperaturas una por una y guardarlas en el vector.
-        //Al final, debe recorrer el vector para calcular y mostrar por consola dos cosas:
-        // 1. La temperatura media de esos días. y 2. Cuántos días la temperatura superó los 30°C.
-
+        //Asignacion por teclado (usuario)
         Scanner teclado = new Scanner(System.in);
-
-        double temperaturas[] = new double[5];
-        double sumaTotal = 0.0;
-        int diasCalurosos = 0;
-
-        System.out.println("Sistema de Registro Climático");
-        System.out.println("=================================");
-
-        //Este For es carga de datos
-        for (int i=0; i < temperaturas.length; i++) {
-            System.out.println("Ingrese la temperatura del día " + (i + 1) + ":");
-            temperaturas[i] = Double.parseDouble(teclado.nextLine());
-        }
-
-        //Este sera el bucle for de procesamiento de datos
-        for (int i=0; i < temperaturas.length; i++) {
-            sumaTotal += temperaturas[i];
-
-            if (temperaturas[i] > 30.0) {
-                diasCalurosos++;
+        for (int f=0; f<3; f++) {
+            for (int c=0; c<3; c++) {
+                System.out.println("Ingrese el valor de la posición f: " + f + "c: " +c);
+                matriz [f][c] = Integer.parseInt(String.valueOf(teclado.nextInt()));
             }
         }
 
-        double media = sumaTotal / temperaturas.length;
-
-        System.out.println("\n=================================");
-        System.out.println("RESUMEN ESTADÍSTICO:");
-        System.out.println("La temperatura media fue de: " + media + "°C");
-        System.out.println("Días que superaron los 30°C: " + diasCalurosos);
-
-        teclado.close(); // Liberamos memoria
+        // Recorrido
+        for (int f=0; f<3; f++) {
+            for (int c=0; c<3; c++) {
+                System.out.print(matriz[f][c] + " ");
+            }
+            System.out.println();
+        }
+        teclado.close();
     }
 }
